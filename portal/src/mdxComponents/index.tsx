4e822f8570e8f0403ff1641a905a6c7ee962f1f1
lfbergee <leiv.fredrik.berge@rikstv.no>
2021-07-08 14:40:55 +0200

    fix: add max length

--- portal/src/mdxComponents/index.tsx
+++ portal/src/mdxComponents/index.tsx
@@ -1,13 +1,15 @@
 import { CodeBlock } from "./CodeBlock/CodeBlock";
 import { H1, H2, H3, H4, Body, Bold } from "@rikstv/shared-components/src/components/typography/Typography";
 
+import "./style.scss";
+
 /* eslint-disable @typescript-eslint/no-explicit-any, react/display-name */
 export const mdxComponents = {
   h1: (props: any): JSX.Element => <H1 {...props} />,
   h2: (props: any): JSX.Element => <H2 {...props} />,
   h3: (props: any): JSX.Element => <H3 {...props} />,
   h4: (props: any): JSX.Element => <H4 {...props} />,
-  p: (props: any): JSX.Element => <Body {...props} />,
+  p: (props: any): JSX.Element => <Body className="portal-max-width" {...props} />,
   b: (props: any): JSX.Element => <Bold {...props} />,
   strong: (props: any): JSX.Element => <Bold {...props} />,
   pre: (props: any): JSX.Element => <div {...props} />,
