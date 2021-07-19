324c90a1a3110dc65ec1553337749aaad0c99c77
lfberge <leiv.fredrik.berge@rikstv.no>
2021-07-19 11:27:43 +0200

    fix: fix typography type, add deploy script

--- shared-components/src/components/typography/Typography.tsx
+++ shared-components/src/components/typography/Typography.tsx
@@ -4,37 +4,42 @@ import "./typography.scss";
 
 type HeadingLevels = 1 | 2 | 3 | 4;
 type SemanticElement = `h${HeadingLevels}` | "p" | "span" | "strong";
-interface Props extends HTMLAttributes<HTMLHeadingElement> {
+type AcceptedHTMLElements = HTMLSpanElement | HTMLHeadingElement | HTMLParagraphElement;
+
+interface Props<T extends AcceptedHTMLElements> extends HTMLAttributes<T> {
   lookLike?: `title-${HeadingLevels}` | "display-title" | "body" | "sub-body" | "tag" | "bold" | "meta";
   className?: string;
 }
 
-interface FactoryProps extends Pick<Props, "lookLike"> {
+interface FactoryProps<T extends AcceptedHTMLElements> extends Pick<Props<T>, "lookLike"> {
   semanticElement: SemanticElement;
 }
 
-const typographyFactory = (
-  factoryProps: FactoryProps
-): ForwardRefExoticComponent<Props & React.RefAttributes<HTMLHeadingElement>> =>
-  // eslint-disable-next-line react/display-name
-  forwardRef<HTMLHeadingElement, Props>(
-    ({ children, lookLike = factoryProps.lookLike, className = "", ...props }, ref) => {
-      const C = factoryProps.semanticElement;
-      return (
-        <C className={`rds-${lookLike} ${className}`} {...props} ref={ref}>
-          {children}
-        </C>
-      );
-    }
-  );
+interface TypographyType {
+  <T extends AcceptedHTMLElements>(props: FactoryProps<T>): ForwardRefExoticComponent<
+    Props<T> & React.RefAttributes<T>
+  >;
+}
 
-export const DisplayTitle = typographyFactory({ semanticElement: "h1", lookLike: "display-title" });
-export const H1 = typographyFactory({ semanticElement: "h1", lookLike: "title-1" });
-export const H2 = typographyFactory({ semanticElement: "h2", lookLike: "title-2" });
-export const H3 = typographyFactory({ semanticElement: "h3", lookLike: "title-3" });
-export const H4 = typographyFactory({ semanticElement: "h4", lookLike: "title-4" });
-export const Body = typographyFactory({ semanticElement: "p", lookLike: "body" });
-export const SubBody = typographyFactory({ semanticElement: "p", lookLike: "sub-body" });
-export const Tag = typographyFactory({ semanticElement: "span", lookLike: "tag" });
-export const Bold = typographyFactory({ semanticElement: "strong", lookLike: "bold" });
-export const Meta = typographyFactory({ semanticElement: "span", lookLike: "meta" });
+const typographyFactory: TypographyType = (factoryProps) =>
+  // eslint-disable-next-line react/display-name
+  forwardRef(({ children, lookLike = factoryProps.lookLike, className = "", ...props }, ref) => {
+    const C = factoryProps.semanticElement;
+    return (
+      // @ts-ignore ts mismatches ref while constrained
+      <C className={`rds-${lookLike} ${className}`} {...props} ref={ref}>
+        {children}
+      </C>
+    );
+  });
+
+export const DisplayTitle = typographyFactory<HTMLHeadingElement>({ semanticElement: "h1", lookLike: "display-title" });
+export const H1 = typographyFactory<HTMLHeadingElement>({ semanticElement: "h1", lookLike: "title-1" });
+export const H2 = typographyFactory<HTMLHeadingElement>({ semanticElement: "h2", lookLike: "title-2" });
+export const H3 = typographyFactory<HTMLHeadingElement>({ semanticElement: "h3", lookLike: "title-3" });
+export const H4 = typographyFactory<HTMLHeadingElement>({ semanticElement: "h4", lookLike: "title-4" });
+export const Body = typographyFactory<HTMLParagraphElement>({ semanticElement: "p", lookLike: "body" });
+export const SubBody = typographyFactory<HTMLParagraphElement>({ semanticElement: "p", lookLike: "sub-body" });
+export const Tag = typographyFactory<HTMLParagraphElement>({ semanticElement: "span", lookLike: "tag" });
+export const Bold = typographyFactory<HTMLParagraphElement>({ semanticElement: "strong", lookLike: "bold" });
+export const Meta = typographyFactory<HTMLSpanElement>({ semanticElement: "span", lookLike: "meta" });
