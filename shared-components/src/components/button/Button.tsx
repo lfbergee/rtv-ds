f24105b1354efecb845e40a0be3e7fc5d7b95838
Adam <adamandersson89@gmail.com>
2021-09-02 16:17:10 +0200

    Adds ref to Basebutton

--- shared-components/src/components/button/Button.tsx
+++ shared-components/src/components/button/Button.tsx
@@ -5,13 +5,13 @@ import "./button.scss";
 import "./button-shared.scss";
 
 export const PrimaryButton: FC<ButtonProps> = (props) => (
-  <BaseButton buttonStyle="rds-button" buttonType="primary" {...props} />
+  <BaseButton buttonStyle="rds-button" buttonType="primary" ref={props.ref} {...props} />
 );
 
 export const SecondaryButton: FC<ButtonProps> = (props) => (
-  <BaseButton buttonStyle="rds-button" buttonType="secondary" {...props} />
+  <BaseButton buttonStyle="rds-button" buttonType="secondary" ref={props.ref} {...props} />
 );
 
 export const TertiaryButton: FC<ButtonProps> = (props) => (
-  <BaseButton buttonStyle="rds-button" buttonType="tertiary" {...props} />
+  <BaseButton buttonStyle="rds-button" buttonType="tertiary" ref={props.ref} {...props} />
 );
