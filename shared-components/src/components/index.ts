e8bbe7a9a393fc343e1e33025aab48e7951ab875
Adam <adamandersson89@gmail.com>
2021-10-07 16:24:24 +0200

    feat: add input component [release]

--- shared-components/src/components/index.ts
+++ shared-components/src/components/index.ts
@@ -8,5 +8,6 @@ export { PrimaryIconButton, SecondaryIconButton, TertiaryIconButton } from "./bu
 export { ToggleButton } from "./toggleButton/ToggleButton";
 export { RadioToggleInput } from "./toggle/RadioToggle";
 export { FieldSet } from "./toggle/FieldSet";
+export { TextField } from "./textfield/TextField";
 
 export { DisplayTitle, H1, H2, H3, H4, Body, Bold, SubBody, Tag, Meta } from "./typography/Typography";
---
+++ shared-components/src/components/textfield/TextField.tsx
@@ -0,0 +1,70 @@
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from "react";
+import { SubBody } from "..";
+import { generateId } from "../core/utils/generateId";
+
+import "./textfield.scss";
+
+type Props = Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "ref">;
+
+export const TextField = forwardRef<
+  HTMLInputElement,
+  Props & {
+    className?: string;
+    name: string;
+    label: string;
+    fullWidth?: boolean;
+    lightBackground?: boolean;
+    labelClass?: string;
+    error?: string | null | false;
+  }
+>(
+  (
+    {
+      className = "",
+      id,
+      labelClass = "",
+      type = "text",
+      fullWidth = false,
+      lightBackground = false,
+      label,
+      error,
+      ...props
+    },
+    ref
+  ) => {
+    const errorTextId = generateId();
+    const generatedId = generateId(id);
+
+    return (
+      <div className={`rds-form-group ${className}`}>
+        <input
+          ref={ref}
+          id={generatedId}
+          className={`
+        rds-textfield
+        rds-textfield--input ${error ? "rds-textfield--input--error" : ""} ${
+            fullWidth ? "rds-textfield--input--full-width" : ""
+          } ${lightBackground ? "rds-light" : ""}
+        `}
+          type={type}
+          placeholder={label}
+          aria-describedby={error ? errorTextId : undefined}
+          {...props}
+        />
+        {error && (
+          <SubBody id={errorTextId} className="rds-textfield--error-text">
+            {error}
+          </SubBody>
+        )}
+        <label
+          htmlFor={generatedId}
+          className={`rds-textfield rds-textfield--label ${labelClass} ${lightBackground ? "rds-light" : ""}`}
+        >
+          <span>{label}</span>
+        </label>
+      </div>
+    );
+  }
+);
+
+TextField.displayName = "TextField";
