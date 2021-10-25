5b5de3b4fc86dc3251d388a9945e8b29c92b8544
Adam <adamandersson89@gmail.com>
2021-10-25 15:35:19 +0200

    uses generated id:s and imports svg:s as base64

--- shared-components/src/components/checkbox/Checkbox.tsx
+++ shared-components/src/components/checkbox/Checkbox.tsx
@@ -1,34 +1,39 @@
 import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from "react";
 
 import "./checkbox.scss";
+import { generateId } from "./internal/generateId";
 
 export interface Checkbox extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   lightBackground: boolean;
   labelText: string;
-  id: string;
+  id?: string;
 }
 
 type Props = Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & Checkbox, "ref">;
 
 export const Checkbox = forwardRef<HTMLInputElement, Props>(
-  ({ className, id, lightBackground, labelText, ...props }, ref) => (
-    <div className="rds-form-group">
-      <input
-        ref={ref}
-        className={`
+  ({ className, id, lightBackground, labelText, ...props }, ref) => {
+    const internalId = generateId(id);
+
+    return (
+      <div className="rds-form-group">
+        <input
+          ref={ref}
+          className={`
           rds-checkbox
           rds-checkbox--input ${className} ${lightBackground ? "rds-light" : ""}
           `}
-        id={id}
-        type="checkbox"
-        {...props}
-      />
-      <label htmlFor={id} className="rds-checkbox--label">
-        {labelText}
-      </label>
-    </div>
-  )
+          id={internalId}
+          type="checkbox"
+          {...props}
+        />
+        <label htmlFor={internalId} className="rds-checkbox--label">
+          {labelText}
+        </label>
+      </div>
+    );
+  }
 );
 
 Checkbox.displayName = "Checkbox";
