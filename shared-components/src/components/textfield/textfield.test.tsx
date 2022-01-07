2c081489870c9b023afadf2eeead72b1c8ac57a0
Adam <adamandersson89@gmail.com>
2022-01-07 16:20:51 +0100

    updates tests with new class names

--- shared-components/src/components/textfield/textfield.test.tsx
+++ shared-components/src/components/textfield/textfield.test.tsx
@@ -13,24 +13,24 @@ brands.forEach((brand) => {
           className="password-input-class"
           label="Password"
           type="password"
-          error="Wrong password"
+          errorMessage="Wrong password"
         />
       </>
     );
-    cy.get("div").should("have.class", "rds-form-group").and("have.class", "username-input-class");
-    cy.get("input").first().should("have.class", "rds-textfield").and("have.class", "rds-textfield--input");
+    cy.get("div").should("have.class", "rds-textfield").and("have.class", "username-input-class");
+    cy.get("input").first().should("have.class", "rds-textfield-input");
     cy.get("input").first().should("have.id", "username-input");
     cy.get("input").first().should("have.attr", "name", "username");
     cy.get("input").first().should("have.attr", "placeholder", "Username");
     cy.get("input").first().should("have.attr", "type", "text");
-    cy.get("label").first().should("have.class", "rds-textfield").and("have.class", "rds-textfield--label");
+    cy.get("label").first().should("have.class", "rds-textfield-label");
 
-    cy.get("div").should("have.class", "rds-form-group").and("have.class", "password-input-class");
-    cy.get("input").last().should("have.class", "rds-textfield").and("have.class", "rds-textfield--input");
+    cy.get("div").should("have.class", "rds-textfield").and("have.class", "password-input-class");
+    cy.get("input").last().should("have.class", "rds-textfield-input");
     cy.get("input").last().should("have.attr", "name", "password");
     cy.get("input").last().should("have.attr", "placeholder", "Password");
     cy.get("input").last().should("have.attr", "type", "password");
-    cy.get("label").last().should("have.class", "rds-textfield").and("have.class", "rds-textfield--label");
+    cy.get("label").last().should("have.class", "rds-textfield-label");
     cy.get("div")
       .should("have.class", "password-input-class")
       .children()
